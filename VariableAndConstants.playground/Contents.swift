6402b55dbef98be8b27cf9a53f33418ab0fcb41b
abdymalikmulky <abdymalikmulky@gmail.com>
2017-02-23 22:21:21 +0700

    comment

--- VariableAndConstants.playground/Contents.swift
+++ VariableAndConstants.playground/Contents.swift
@@ -2,8 +2,10 @@ import UIKit
 
 //Constanta == immutable
 let birthYear = 1993
+birthYear = 1995 //this will show you an error | error: cannot assign to value: 'birthYear' is a 'let' constant
 
 //Variables == mutable
 var currentYear = 2017
 var age = 23
-age = currentYear - birthYear
+age = currentYear - birthYear //success 😀 | result 23
+
